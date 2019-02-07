0f8c73b9156ce782f14e677d147fcc1954848dd3
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-07 14:37:07 +0530

    Create set3.c

---
+++ set3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n,k,sum=0;
+printf("Enter the values:");
+scanf("%d"\t"%d",&n,&k);
+for(i=1;i<=n;i++)
+{
+printf("%d\n",&n);
+}
+for(j=1;j<=n;j++)
+{
+sum=sum+j;
+}
+printf("sum is %d\n",sum);
+getch();
+}
